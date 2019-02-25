ed7681ba1e9708178c552d1a3ff42a3dc4e650c5
Manny <coachmannytw@gmail.com>
2019-02-24 21:34:46 -0700

    updated css

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import "./App.css";
 class App extends Component {
   //this is where you set your initial state
   state = {
-    name: '',
-    lastName: '',
+    name: "",
+    lastName: "",
     info: []
   };
 
-  // this is your event handler, this function uses the submit event 
-  // listener to call the function. 
+  // this is your event handler, this function uses the submit event
+  // listener to call the function.
   // e.preventDefault prevents React from refreshing the page each time state
   // changes
   handleSubmit = e => {
     e.preventDefault();
-  // declare a variable "data" and assign it to be set to a string
-  // use the spread operator "..." to take in the all of the information from
-  // the form and set it to state 
+    // declare a variable "data" and assign it to be set to a string
+    // use the spread operator "..." to take in the all of the information from
+    // the form and set it to state
     const data = JSON.stringify({ ...this.state });
-  // this is how you would wet up a post request to your api
+    // this is how you would wet up a post request to your api
     fetch("http://localhost:4000", {
       method: "POST",
       body: data,
@@ -28,9 +28,8 @@ class App extends Component {
         "Content-Type": "application/json"
       }
     })
-    //this will refresh the window once you hit submit
-    .then( () => window.location.reload(true))
-    
+      //this will refresh the window once you hit submit
+      .then(() => window.location.reload(true));
   };
 
   //see article on componentDidMount vs componentWillMount
@@ -38,21 +37,19 @@ class App extends Component {
   //this will send a get request to the address in the fetch
   //it will then return the results and set the results to JSON
   //it will then take those results "data" and set them to state ==> info: data
-  componentDidMount(){
-    return fetch('http://localhost:4000')
-    .then(results => {
-      return results.json();
-    })
-    .then(data => {
-      this.setState({info: data})
-    })
+  componentDidMount() {
+    return fetch("http://localhost:4000")
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({ info: data });
+      });
   }
- 
 
   render() {
-
     // this is one way to map the data, it goes in the render
-    // it maps the info and sets it to a varialbe "myList" 
+    // it maps the info and sets it to a varialbe "myList"
     const myList = this.state.info.map(person => {
       return (
         <div>
@@ -62,46 +59,39 @@ class App extends Component {
     });
     //this will show you state and see the data put into "info"
     //you can remove the console log, this is just to see the state initially
-    console.log("info:",this.state.info)
+    console.log("info:", this.state.info);
     return (
       <div className="App">
         <h1>hello from react</h1>
 
-      {/* This is the form with that will trigger the submit event listenter */}
-      {/* This form is taken directly from bootstrap */}
+        {/* This is the form with that will trigger the submit event listenter */}
+        {/* This form is taken directly from bootstrap */}
         <form onSubmit={this.handleSubmit}>
-          <div class="form-group">
+          <div classname="form-group">
             <label htmlFor="exampleInput">Name</label>
             <input
               type="text"
               className="form-control"
               id="exampleInput"
               placeholder="Enter Name"
-              // the onchange uses an event listener to set state "name" and uses 
+              // the onchange uses an event listener to set state "name" and uses
               // e.target.value to take the input from the form and set that to state
               onChange={e => this.setState({ name: e.target.value })}
             />
-           
           </div>
 
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" classname="btn btn-primary">
             Submit
           </button>
 
           <div>
             {/* within {} we can call our map function to display our data that has been mapped */}
-            <h3>{ myList }</h3>
+            <h3>{myList}</h3>
           </div>
         </form>
-       
-
-          <div>
-            map
-          </div>
       </div>
     );
   }
 }
 
 export default App;
-
